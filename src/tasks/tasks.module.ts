b76ee2c7d3e210aaaa44e49b8f5fee7f7b9f1130
zanwalker-dev <andre.rocha2302@gmail.com>
2025-10-14 16:26:49 -0300

    generate right service tasks

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 
 @Module({
   imports: [],
   controllers: [TasksController],
-  providers: [],
+  providers: [TasksService],
 })
 export class TasksModule {}
