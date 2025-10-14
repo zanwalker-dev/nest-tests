622d3bb6247e0838a833f454b47258b2cc07e7c4
zanwalker-dev <andre.rocha2302@gmail.com>
2025-10-14 16:19:22 -0300

    generating it with cli

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TasksController } from './tasks.controller';
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [TasksController],
 })
 export class TasksModule {}
