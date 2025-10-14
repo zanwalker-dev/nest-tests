4d4b70484881468a33215199263d1220a25830f0
zanwalker-dev <andre.rocha2302@gmail.com>
2025-10-14 15:31:01 -0300

    deleting users to test create it with cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
