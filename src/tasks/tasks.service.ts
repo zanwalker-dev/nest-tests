f8570c9ec548deac6ca4986a8dcd393a679b05da
zanwalker-dev <andre.rocha2302@gmail.com>
2025-10-14 16:29:30 -0300

    creating a service to test

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  getTasks() {
+    return [{ id: 1, task: 'comprar pão' }];
+  }
+}
