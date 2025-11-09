c176151a830e5e114ae8291290c43c3f6d90b650
zanwalker-dev <andre.rocha2302@gmail.com>
2025-11-09 09:40:30 -0300

    feat:apllying validations on create task dto

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString()
+  @IsEmpty()
+  @MinLength(5)
   readonly name: string;
+
   readonly description: string;
 }
